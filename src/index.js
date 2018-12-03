c218e9519af4ae722652d7d1233f8c48501722ff
Victor Hermes <me.victorhermes@gmail.com>
2018-12-02 22:01:14 -0200

    Propriedades

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import { render } from "react-dom";
 
 class Button extends Component {
   render() {
-    return <a href="">Salvar</a>;
+    return (
+      <a href="" onClick={this.props.onClick}>
+        {this.props.children}
+      </a>
+    );
   }
 }
 
 class App extends Component {
+  handleClick() {
+    alert("btn clicado");
+  }
   render() {
     return (
       <Fragment>
         <h1>Hello Moom</h1>
-        <Button />
+        <Button onClick={this.handleClick}>Enviar :)</Button>
       </Fragment>
     );
   }
